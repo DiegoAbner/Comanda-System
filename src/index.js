d7dad518c3e551827021a7f6a724a1b3c5848641
DiegoAbner <diego86abner@gmail.com>
2024-04-01 20:17:47 -0300

    the last changes

--- src/index.js
+++ src/index.js
@@ -1,38 +1,38 @@
 const prompt = require('prompt-sync')({sigint:true});
 
-// Criei esse array para funcionar como armazenamento do carrinho
-let carrinho = [];
+// Criei esse array para funcionar como armazenamento da mesa do restaurante
+let mesa = [];
 
-// SEGUNDA TELA - Função para adicionar um produto, preço e quantidade ao carrinho
+// SEGUNDA TELA - Função para adicionar um cliente e valor total de sua comanda
 
-function adicionarProduto() {
-    let nome = prompt("INFORME O NOME DO CLIENTE:");
-    let preco = parseFloat(prompt("INFORME O VALOR DA COMANDA:"));  //utilizei o parseFloat para converter a string em numero decimal
+function adicionarCliente() {
+    let nome = prompt("INFORME O NOME DO CLIENTE: ");
+    let preco = parseFloat(prompt("INFORME O VALOR DA COMANDA: "));  //utilizei o parseFloat para converter a string em numero decimal
     let quantidade = parseInt(prompt("Informe a quantidade do produto:")); //utilizei o parseInt para converter a string em numeros inteiros
 
     // Calcula o subtotal do produto
     let subtotal = preco * quantidade;
 
     // Adiciona o produto ao carrinho
-    carrinho.push({ nome, preco, quantidade, subtotal });
+    mesa.push({ nome, preco, quantidade, subtotal });
 
   alert(`Produto: ${nome}, foi adicionado ao carrinho.`); 
   alert(`Valor por item: R$ ${preco} \n`);
 }
 
 // Função para exibir os itens do carrinho e o total da compra
-function exibirCarrinho() {
+function exibirMesa() {
     let mensagem = "Produtos no carrinho:";
 
     // Itera sobre os produtos no carrinho
-    carrinho.forEach(produto => {
+    mesa.forEach(produto => {
         mensagem += `\nNome: ${produto.nome}\n`;
         mensagem += `Preço unitário: R$ ${produto.preco.toFixed(2)}\n`;
         mensagem += `Subtotal: R$ ${produto.subtotal.toFixed(2)}\n`;
     });
 
     // Para calcular o valor total da compra
-    let total = carrinho.reduce((acc, produto) => acc + produto.subtotal, 0);//pesquisar reduce map filter
+    let total = mesa.reduce((acc, produto) => acc + produto.subtotal, 0);//pesquisar reduce map filter
 
     mensagem += `Total da compra: R$ ${total.toFixed(2)}\n`;
 
@@ -42,14 +42,14 @@ function exibirCarrinho() {
 //PRIMEIRA TELA - Função principal para interação com o usuário
 function main() {
     while (true) {
-        let opcao = prompt("Escolha uma opção:\n 1 - Adicionar produto ao carrinho\n 2 - Visualizar carrinho\n 3 - Finalizar compra\n 4 - Sair \n==>");
+        let opcao = prompt("ESCOLHA UMA OPÇÃO:\n \n 1 - Adicionar Cliente à Mesa\n 2 - Visualizar consumo da Mesa\n 3 - Encerrar todas as Comandas\n 4 - Abortar Lançamentos \n DIGITE A OPÇÃO DESEJADA:  ");
 
         switch (opcao) {
             case "1":
-                adicionarProduto();
+                adicionarCliente();
                 break;
             case "2":
-                exibirCarrinho();
+                exibirMesa();
                 break;
             case "3":
                 console.log ("COMPRA FINALIZADA. Supermercados Abner agradece pela compra");
